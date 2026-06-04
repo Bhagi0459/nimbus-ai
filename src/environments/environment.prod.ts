4c7ca71aef38d258ee46e54c6daa17320832bca2
Bhagya sankar Maddela <m.bhagyasankar@gmail.com>
2026-06-04 17:58:18 +0530

    Added env prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  aiApiUrl: 'https://nimbus-ai-api.onrender.com',
+
+  weatherApiUrl: 'https://nimbus-ai-api.onrender.com/api/weather',
+
+  weatherSearchApiUrl: 'https://nimbus-ai-api.onrender.com/api/weather/search',
+};
