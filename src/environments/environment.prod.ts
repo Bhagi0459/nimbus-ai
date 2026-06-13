4bf8ddcde4319bde90448f65ae6aae0d4889c0ba
Bhagya sankar Maddela <m.bhagyasankar@gmail.com>
2026-06-13 12:04:59 +0530

    Fixed Css

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-
   aiApiUrl: 'https://nimbus-ai-api.onrender.com',
-
   weatherApiUrl: 'https://nimbus-ai-api.onrender.com/api/weather',
-
   weatherSearchApiUrl: 'https://nimbus-ai-api.onrender.com/api/weather/search',
 };
